a0011f711fcaa2046c880f469fb2e1cec211356f
Uday Kiran Kavaturu <udaykiran19491@gmail.com>
2024-02-24 07:01:50 +0530

    conditional statements

---
+++ C++/conditional_statements.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int marks = 40;
+    // if(marks>=40){
+    //     cout<<"Passed!";
+    // } else {
+    //     cout<<"Failed!";
+    // }
+
+    // // Grades
+    // if(marks > 80){
+    //     cout<<'A';
+    // } else if(marks >= 60){
+    //     cout<<'B';
+    // } else if(marks >= 35){
+    //     cout<<'C';
+    // } else {
+    //     cout<<'F';
+    // }
+    // cout<<"end of code"<<endl;
+
+
+    // switch
+    // int day = 9;
+    // switch (day){
+    //     case 1: cout<<"Blue";
+    //             break;
+    //     case 2: cout<<"Yellow";
+    //             break;
+    //     case 3: cout<<"Pink";
+    //             break;
+    //     case 4: cout<<"Red";
+    //             break;
+    //     case 5: cout<<"Black";
+    //             break;
+    //     case 6: cout<<"White";
+    //             break;
+    //     case 7: cout<<"Green";
+    //             break;
+    //     default: cout<<"Invalid input";
+    //                     break;
+    // }
+
+
+    // ternary operator
+    int marks = 20;
+    (marks >= 35)?cout<<"Passed":cout<<"Failed";
+}
