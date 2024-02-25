edae2df157216c6487bf3aff44f77f305db16a16
Uday Kiran Kavaturu <udaykiran19491@gmail.com>
2024-02-25 07:03:21 +0530

    loops

---
+++ C++/loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int i = 1;
+    // while(i<=10){
+    //     cout<<i<<endl;
+    //     i = i+1;
+    // }
+
+    // do {
+    //     cout<<i<<endl;
+    //     i++; 
+    // } while(i<=10);
+
+    for (int i = 1;i<=10; i++){
+        cout<<i<<endl;
+    }
+}
