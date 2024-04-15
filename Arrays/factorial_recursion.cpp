2469b9209ad5432c6214aa7a1a857a52296a00df
Uday Kiran Kavaturu <udaykiran19491@gmail.com>
2024-04-15 09:12:20 +0530

    recursion

--- Arrays/factorial_recursion.cpp
+++ Arrays/factorial_recursion.cpp
@@ -3,20 +3,18 @@ using namespace std;
 
 int factorial(int num){
     // base case
-    if(num == 1){
-        return 1;
-    }
+    if(num == 1) return 1;
 
     // recursive case
-    int small_answer = factorial(num-1);
+    int small_ans = factorial(num -1);
 
     // operation
-    int big_answer = num * small_answer;
+    int big_ans = num * small_ans;
 
-    return big_answer;
+    return big_ans;
 }
 
 int main(){
     int result = factorial(6);
-    cout<<"result: "<<result<<endl;
+    cout<<"result "<<result<<endl;
 }
