77f8fed1dacc1712eb97517a7d49de6ee62491e1
Uday Kiran Kavaturu <udaykiran19491@gmail.com>
2024-05-27 06:43:29 +0530

    stacks

--- Stacks/stacks.cpp
+++ Stacks/stacks.cpp
@@ -6,7 +6,7 @@ class Node{
     public:
         T value;
         Node* next;
-    
+
         Node(T value){
             this->value = value;
             this->next = nullptr;
@@ -22,21 +22,22 @@ class Stack{
     public:
         Stack(){
             this->head = nullptr;
-            size = 0;
+            this->size = 0;
         }
 
         void push(T value){
             Node<T>* new_node = new Node<T>(value);
 
             if(this->head == nullptr){
-                head =  new_node;
+                this->head = new_node;
                 size++;
                 return;
             }
 
-            new_node->next = head;
-            head = new_node;
+            new_node->next = this->head;
+            this->head = new_node;
             size++;
+            return;
         }
 
         void pop(){
@@ -45,8 +46,8 @@ class Stack{
                 return;
             }
 
-            Node<T>* temp = head;
-            head = head->next;
+            Node<T>* temp = this->head;
+            this->head = this->head->next;
             temp->next = nullptr;
             delete temp;
             size--;
@@ -58,46 +59,37 @@ class Stack{
                 return;
             }
 
-            cout<<"top element is "<<this->head->value<<endl;
+            cout<<"top element is:"<<this->head->value<<endl;
+            return;
         }
 
-        void empty(){
+        void isEmpty(){
             if(this->head == nullptr){
                 cout<<"stack is empty"<<endl;
                 return;
-            } else{
-                cout<<"stack is not empty"<<endl;
+            } else {
+                  cout<<"stack is not empty"<<endl;
                 return;
             }
         }
 
         void getSize(){
-            if(this->head == nullptr){
-                cout<<"stack is empty"<<endl;
-                return;
-            }
-
-            
-            cout<<"size of stack is:"<<this->size<<endl;
+            cout<<"size of stack is: "<<this->size<<endl;
         }
 };
 
 int main(){
-
-    Stack<int>* s = new Stack<int>();
-
-
-    s->push(10); 
-    s->push(20); 
-    s->getSize(); 
-    s->top(); 
-    s->pop();  
-    s->getSize(); 
-    s->top(); 
-    s->empty(); 
-    s->pop(); 
-    s->getSize();
-    s->empty(); 
-
+    Stack<char> s;
+    s.push('a'); // 10
+    s.push('b'); // 20 -> 10
+    s.push('c'); // 30 -> 20 -> 10
+    s.getSize(); // 3
+    s.isEmpty(); //not empty
+    s.pop(); // 20 -> 10
+    s.getSize(); // 2
+    s.pop(); // 10
+    s.top(); // 10
+    s.pop(); // 
+    s.isEmpty(); // empty
 
 }
